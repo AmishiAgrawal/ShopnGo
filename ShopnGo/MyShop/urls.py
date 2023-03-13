66f93d13cc4cb960150248b19858d6090fa50505
amishi <amiagrwal2002@gmail.com>
2023-03-13 17:12:40 +0530

    header Changes

--- ShopnGo/MyShop/urls.py
+++ ShopnGo/MyShop/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.index)
+    path('',views.index),
+    path('home',views.index),
+    path('contact',views.contact),
+    path('about',views.about),
+    path('cart',views.cart),
+    path('wishlist',views.wishlist),
+    path('categories',views.categories),
+    # path('',views.index),
+    # path('',views.index),
 ]
--- ShopnGo/MyShop/views.py
+++ ShopnGo/MyShop/views.py
@@ -2,3 +2,18 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'MyShop/index.html')
+
+def about(request):
+    return render(request,'MyShop/about.html')
+
+def contact(request):
+    return render(request,'MyShop/contact.html')
+
+def wishlist(request):
+    return render(request,'MyShop/wishlist.html')
+
+def cart(request):
+    return render(request,'MyShop/cart.html')
+
+def categories(request):
+    return render(request,'MyShop/categories.html')
